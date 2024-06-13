0a12ec9f3c4b49150bc5543198744a404574a553
seungheon <77201925+peaceseungheon@users.noreply.github.com>
2024-06-14 00:14:41 +0900

    오늘의 웹툰 프로젝트 생성 및 api 통신 서비스 클래스 개발

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          "오늘의 웹툰",
+          style: TextStyle(
+            fontSize: 24,
+          ),
+        ),
+        elevation: 2,
+        foregroundColor: Colors.green,
+        backgroundColor: Colors.white,
+      ),
+    );
+  }
+}
